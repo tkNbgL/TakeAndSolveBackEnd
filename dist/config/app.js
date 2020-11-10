41a2b81909936bdfb7c82a14ed06c89a8e498bb9
utku <utku.nebioglu@gmail.com>
2020-11-10 22:15:19 +0300

    initial project structure

---
+++ dist/config/app.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const bodyParser = require("body-parser");
+const test_routes_1 = require("../routes/test_routes");
+const common_routes_1 = require("../routes/common_routes");
+class App {
+    constructor() {
+        this.test_routes = new test_routes_1.TestRoutes();
+        this.common_routes = new common_routes_1.CommonRoutes();
+        this.app = express();
+        this.config();
+        this.test_routes.route(this.app);
+        this.common_routes.route(this.app);
+    }
+    config() {
+        // support application/json type post data
+        this.app.use(bodyParser.json());
+        //support application/x-www-form-urlencoded post data
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+}
+exports.default = new App().app;
